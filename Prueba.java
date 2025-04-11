2da50e20f3aa01423eb7ef773672b2144a1ab4a9
rlopez <ruben.lopez@informatica.palomafp.org>
2025-04-11 10:09:18 +0200

    textos desde main_textos

--- Prueba.java
+++ Prueba.java
@@ -3,7 +3,7 @@ public class Prueba {
     public static void main(String[] args) {
 	    System.out.println("Texto de ejemplo.");
 	    System.out.println("Texto desde branch 'textos_main'.");
-        System.out.println("Texto desde branch 'mas_textos'.");
+        System.out.println("Más texto desde branch 'mas_textos'.");
     }
     
 }
